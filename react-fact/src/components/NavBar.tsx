266953da4997ea744c529c01ef4bc58542ec48e7
keshavarun20 <keshavarun1220@gmail.com>
2025-07-21 18:19:27 +0530

    Add NavBar and Main components; enhance App structure with new styles and fonts

---
+++ react-fact/src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+const NavBar = () => {
+  return (
+    <header className="header">
+      <nav className="navbar">
+        <img className="navbar-logo" src="https://www.pngall.com/wp-content/uploads/15/React-Logo-PNG.png" alt="navbar-logo" />
+        <span className="navbar-text">ReactFacts</span>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
