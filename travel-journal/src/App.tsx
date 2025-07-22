b1225610bad6dd65d038e11e43bf8649696bd4f6
keshavarun20 <keshavarun1220@gmail.com>
2025-07-22 12:26:26 +0530

    initialize travel journal project with React, Vite, and TypeScript

---
+++ travel-journal/src/App.tsx
@@ -0,0 +1,14 @@
+import Entry from "./components/Entry";
+import Header from "./components/Header";
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Entry />
+    </>
+  );
+};
+
+export default App;
---
+++ travel-journal/src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header>
+      <img src="/images/globe.png" alt="globe" />
+      <span>My Travel Journal</span>
+    </header>
+  );
+};
+
+export default Header;
