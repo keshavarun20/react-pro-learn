8f334d935f9cc64d9675ee37f7f914a985ee5eb9
keshavarun20 <keshavarun1220@gmail.com>
2025-07-23 12:10:56 +0530

    Refactor App component: simplify entry rendering by storing mapped entries in a variable

--- travel-journal/src/App.tsx
+++ travel-journal/src/App.tsx
@@ -3,14 +3,12 @@ import Header from "./components/Header";
 import "./App.css";
 import data from "./data";
 const App = () => {
+  const renderEntry = data.map((item) => <Entry key={item.id} {...item} />);
+
   return (
     <>
       <Header />
-      <main className="container">
-        {data.map((item) => (
-        <Entry key={item.id} {...item} />
-      ))}
-      </main>
+      <main className="container">{renderEntry}</main>
     </>
   );
 };
