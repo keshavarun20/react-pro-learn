e8acfd3d5861c1083c21775d002d3fc94d7ec00d
Keshav <keshavarun1220@gmail.com>
2025-08-01 10:13:52 +0530

    Defined Meme generation Template

--- meme-generator/src/components/Main.tsx
+++ meme-generator/src/components/Main.tsx
@@ -1,22 +1,52 @@
+import { useState } from "react";
+type meme = {
+  img: string;
+  topText: string;
+  bottomText: string;
+};
 const Main = () => {
+  const [meme, setMeme] = useState<meme>({
+    img: "http://i.imgflip.com/1bij.jpg",
+    topText: "One does not simply",
+    bottomText: "Walk into Mordor",
+  });
+  
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.currentTarget;
+    setMeme((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
   return (
     <main>
       <div className="form">
         <label>
           Top Text
-          <input type="text" placeholder="One does not simply" name="topText" />
+          <input
+            type="text"
+            placeholder={meme.topText}
+            name="topText"
+            onChange={handleChange}
+          />
         </label>
 
         <label>
           Bottom Text
-          <input type="text" placeholder="Walk into Mordor" name="bottomText" />
+          <input
+            type="text"
+            placeholder={meme.bottomText}
+            name="bottomText"
+            onChange={handleChange}
+          />
         </label>
         <button>Get a new meme image 🖼</button>
       </div>
       <div className="meme">
-        <img src="http://i.imgflip.com/1bij.jpg" alt="Meme Image" />
-        <span className="top">One does not simply</span>
-        <span className="bottom">Walk into Mordor</span>
+        <img src={meme.img} alt="Meme Image" />
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
       </div>
     </main>
   );
