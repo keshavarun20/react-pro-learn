907f1a3950bca9522898cf2bb53360b3041562fc
keshavarun20 <keshavarun1220@gmail.com>
2025-08-03 12:59:39 +0530

    Fixed errors

--- chef-claude/src/App.tsx
+++ chef-claude/src/App.tsx
@@ -1,13 +1,11 @@
 import Header from "./components/Header";
 import "./App.css";
 import Main from "./components/Main";
-import { Analytics } from "@vercel/analytics/next";
 const App = () => {
   return (
     <>
       <Header />
       <Main />
-      <Analytics />
     </>
   );
 };
