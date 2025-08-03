78e743c6327d826ded1735859d5859871310d20d
keshavarun20 <keshavarun1220@gmail.com>
2025-08-03 12:57:55 +0530

    Added Vercel Analytics

--- chef-claude/src/App.tsx
+++ chef-claude/src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import "./App.css";
 import Main from "./components/Main";
+import { Analytics } from "@vercel/analytics/next";
 const App = () => {
   return (
     <>
       <Header />
-      <Main/>
+      <Main />
+      <Analytics />
     </>
   );
 };
